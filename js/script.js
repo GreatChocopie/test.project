0115f8941a8325cab7f8fdf17104bb5375e370d4
Milada <greatchocopie@yandex.com>
2020-11-02 23:57:19 +0800

    попап начало

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var topMenu = document.querySelector(".header");
 var menuButton = topMenu.querySelector(".header__nav-menu-open");
 var menuOpen = topMenu.querySelector(".header__nav-menu");
 var body = document.querySelector("body");
+var numberButton = topMenu.querySelector(".desctop-number__text");
+var numberToltip = document.querySelector(".desctop-number__toltip-wrapper")
 
 
 
@@ -13,4 +15,7 @@ menuButton.addEventListener("click", function (evt) {
   topMenu.classList.toggle("header_menu-js");
   menuButton.classList.toggle("header__nav-menu-open_close");
   body.classList.toggle("modal-open-js");
+});
+numberButton.addEventListener("click", function (evt) {
+  numberToltip.classList.toggle("desctop-number__toltip_open-js");
 });
