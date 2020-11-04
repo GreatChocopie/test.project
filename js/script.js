6f085b8f769815710e2d9a587136b8fd8bae4499
Milada <greatchocopie@yandex.com>
2020-11-04 22:21:39 +0800

    мобил меню с категориями

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var topMenu = document.querySelector(".header");
 var menuButton = topMenu.querySelector(".header__nav-menu-open");
+var categoryButton = topMenu.querySelector(".header__category-open");
+var categoryOpen  = topMenu.querySelector(".header__category-container");
+var categoryClose = categoryOpen.querySelector(".header__category-close");
 var menuOpen = topMenu.querySelector(".header__nav-menu");
 var body = document.querySelector("body");
 var numberButton = topMenu.querySelector(".desctop-number__text");
@@ -42,6 +45,15 @@ window.addEventListener('scroll', function(e) {
 }
 
 
+// function elemHasParent(elem, needle) {
+//   while (elem != null) {
+//     elem = elem.parentNode;
+//     if (elem == needle)
+//       return true;
+//   }
+//   return false;
+// }
+
 
 
 
@@ -49,14 +61,23 @@ window.addEventListener('scroll', function(e) {
 
 buttonSearch.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popapSearch.classList.toggle("header__search-form-open-js");
+  popapSearch.classList.toggle("modal-open-js");
 });
 menuButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menuOpen.classList.toggle("header__nav-menu_open-js");
+  menuOpen.classList.toggle("modal-open-js");
   menuButton.classList.toggle("header__nav-menu-open_close");
-  body.classList.toggle("modal-open-js");
-
+  body.classList.toggle("modal-open-js_body");
+});
+categoryButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  categoryOpen.classList.add("modal-open-js");
+  body.classList.add("modal-open-js_body");
+});
+categoryClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  categoryOpen.classList.remove("modal-open-js");
+  body.classList.remove("modal-open-js_body");
 });
 numberButton.addEventListener("click", function (evt) {
   numberToltip.classList.toggle("desctop-number__toltip_open-js");
