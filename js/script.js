ab2108b97ec495e76bd130a790e5b61df294d741
Milada <greatchocopie@yandex.com>
2020-11-04 11:53:39 +0800

    скролинг

--- js/script.js
+++ js/script.js
@@ -10,13 +10,43 @@ var popapSearch = document.querySelector(".header__search-form");
 var windowWidth = window.innerWidth;
 
 
+
+
+
+
 if (windowWidth > 767) {
   const details = document.querySelectorAll("details");
 
   for (detail of details) {
     detail.open = true;
-  }
+  };
+
+  let prevY = 0;
+
+window.addEventListener('scroll', function(e) {
+    let elem = document.querySelector(".header__category-wrapper"); // элемент который хотим спрятать
+    let currentY = window.scrollY;
+    if (currentY > prevY) {
+	// прокрутка вниз
+	// console.log('down');
+	elem.style.margin = "-130px 0 0 0";
+    }
+    else {
+	// прокрутка вверх, возвращение назад
+	console.log('up')
+	elem.style.margin = "0";
+    }
+    prevY = currentY;
+});
+
 }
+
+
+
+
+
+
+
 buttonSearch.addEventListener("click", function (evt) {
   evt.preventDefault();
   popapSearch.classList.toggle("header__search-form-open-js");
@@ -27,18 +57,9 @@ menuButton.addEventListener("click", function (evt) {
   topMenu.classList.toggle("header_menu-js");
   menuButton.classList.toggle("header__nav-menu-open_close");
   body.classList.toggle("modal-open-js");
+  
 });
 numberButton.addEventListener("click", function (evt) {
   numberToltip.classList.toggle("desctop-number__toltip_open-js");
   background.classList.toggle("background-js-open")
 });
-
-// window.addEventListener('scroll', function (e) {
-
-
-
-
-
-
-
-// });
