1a0601a7d2301660489740f27701c72a70689627
Milada <greatchocopie@yandex.com>
2020-11-06 14:25:14 +0800

    свайпер

--- js/script.js
+++ js/script.js
@@ -87,24 +87,3 @@ numberButton.addEventListener("click", function (evt) {
 
 
 
-
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 1,
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  
-})
